b4d7ff0b069e1ce6be6869e50cffcd3697ef3856
Vnsgu123 <111989739+Vnsgu123@users.noreply.github.com>
2022-09-21 21:17:34 +0530

    Update promblemm10.py

--- promblemm10.py
+++ promblemm10.py
@@ -2,7 +2,6 @@ try:
     # cook your dish here
     t=int(input())
     # n=int(input())
-    
     def ADD(z,items):
         u=z.split()
         u[2]=int(u[2])
@@ -11,15 +10,15 @@ try:
             if u[1]==items[i][0]:
                 flag=1
                 items[i][1]=items[i][1]+u[2]
-                print("UPDATED Item ",u[1])
+                print("UPDATED Item",u[1])
         if flag == 0:
             u[2]=str(u[2])
             l=u[1]+" "+u[2]
             # print(l)
-            # k=l.split()
-            items.append(l)
-            # k[1]=int(k[1])
-            print("ADDED Item ",u[1])
+            k=l.split()
+            items.append(k)
+            k[1]=int(k[1])
+            print("ADDED Item",u[1])
             
     def DELETE(z,items):
         u=z.split()
@@ -28,13 +27,12 @@ try:
         for i in range(0,len(items)):
             if u[1]==items[i][0]:
                 flag=1
-                items[i][1]=int(items[i][1])
                 if u[2] <= items[i][1] :
                     items[i][1]=items[i][1]-u[2]
-                    print("DELETED Item ",u[1])
+                    print("DELETED Item",u[1])
                 # elif u[2]==items[i][1]:
                 #     items.remove(items[i])
-                #     print("DELETED Item ",u[1])
+                #     print("DELETED Item",u[1])
                 else :
                     print("Item "+ u[1] + " could not be DELETED")
         if flag == 0 :
@@ -53,7 +51,7 @@ try:
         for i in range(0,n):
             x = input()
             y = x.split()
-            # y[1]=int(y[1])
+            y[1]=int(y[1])
             items.append(y)
         # print(items)
         m=int(input())
