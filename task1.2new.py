40a30a84ed051c90584df1846897dad1f26db474
Vnsgu123 <111989739+Vnsgu123@users.noreply.github.com>
2022-09-27 19:53:34 +0530

    Add files via upload

--- task1.2new.py
+++ task1.2new.py
@@ -1,24 +1,24 @@
 import cv2
 import numpy as np
 
-img=cv2.imread('maze_2.png')
+img=cv2.imread('maze_14.png')
 # cv2.imshow('imge',img)
 cv2.waitKey(0)
 list=[]
 # q=100
 # w=200
-qq=95
-ww=107
+q=100
+w=200
 for j in range(1,7):
-    q=100
-    w=200
+    qq=96
+    ww=105
         
-    for i in range(1,7):
+    for i in range(1,8):
         
-        crop_img=img[q:w ,qq:ww]
+        crop_img=img[qq:ww ,q:w]
         # cv2.imshow('cropimge',crop_img)
         cv2.waitKey(2)
-        gray = cv2.cvtColor(crop_img ,cv2.COLOR_RGB2HSV)
+        gray = cv2.cvtColor(crop_img,cv2.COLOR_RGB2HSV)
         mask = cv2.inRange(gray ,(0,0,231),(180,18,255))
         res = cv2.bitwise_and(crop_img ,crop_img, mask=mask)
         # cv2.imshow('res image ',res)
@@ -33,44 +33,44 @@ for j in range(1,7):
             approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
             cv2.drawContours(crop_img, [approx], 0, (0, 0, 255), 2)
             # cv2.imshow('contour',crop_img)
-            k=i
-            l=i+1
+            
             
             if j==1 :    
-                u='A'+str(k)
-                z='A'+str(l)
+                u='A'+str(i)
+                z='B'+str(i)
                 v=u+'-'+z
             elif j==2 :
-                u='B'+str(k)
-                z='B'+str(l)
+                u='B'+str(i)
+                z='C'+str(i)
                 v=u+'-'+z
             elif j==3 :
-                u='C'+str(k)
-                z='C'+str(l)
+                u='C'+str(i)
+                z='D'+str(i)
                 v=u+'-'+z
             elif j==4 :
-                u='D'+str(k)
-                z='D'+str(l)
+                u='D'+str(i)
+                z='E'+str(i)
                 v=u+'-'+z
             elif j==5 :
-                u='E'+str(k)
-                z='E'+str(l)
+                u='E'+str(i)
+                z='F'+str(i)
                 v=u+'-'+z
             elif j==6 :
-                u='F'+str(k)
-                z='F'+str(l)
+                u='F'+str(i)
+                z='G'+str(i)
                 v=u+'-'+z
+                
             # print(v)
             list.append(v)
             # print(list)
         
 
 
-        q=q+100
-        w=w+100
-    qq=qq+100
-    ww=ww+100
-print("'vertical_road_under_construction':",end ='')
+        qq=qq+100
+        ww=ww+100
+    q=q+100
+    w=w+100
+print("'horizontal_road_under_construction':",end ='')
 print(list)
 
 cv2.waitKey(0)
