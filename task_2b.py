4ea5f63c43dbd8ce621775392d517d3c70a6f4fe
Vnsgu123 <111989739+Vnsgu123@users.noreply.github.com>
2022-10-30 17:31:36 +0530

    Add files via upload

---
+++ task_2b.py
@@ -0,0 +1,191 @@
+'''
+*****************************************************************************************
+*
+*        =================================================
+*             Pharma Bot Theme (eYRC 2022-23)
+*        =================================================
+*                                                         
+*  This script is intended for implementation of Task 2B   
+*  of Pharma Bot (PB) Theme (eYRC 2022-23).
+*
+*  Filename:			task_2b.py
+*  Created:				
+*  Last Modified:		8/10/2022
+*  Author:				e-Yantra Team
+*  
+*  This software is made available on an "AS IS WHERE IS BASIS".
+*  Licensee/end user indemnifies and will keep e-Yantra indemnified from
+*  any and all claim(s) that emanate from the use of the Software or 
+*  breach of the terms of this agreement.
+*
+*****************************************************************************************
+'''
+
+# Team ID:			[ Team-ID ]
+# Author List:		[ Names of team members worked on this file separated by Comma: Name1, Name2, ... ]
+# Filename:			task_2b.py
+# Functions:		control_logic, read_qr_code
+# 					[ Comma separated list of functions in this file ]
+# Global variables:	
+# 					[ List of global variables defined in this file ]
+
+####################### IMPORT MODULES #######################
+## You are not allowed to make any changes in this section. ##
+##############################################################
+import  sys
+import traceback
+import time
+import os
+import math
+from zmqRemoteApi import RemoteAPIClient
+import zmq
+import numpy as np
+import cv2
+import random
+from pyzbar.pyzbar import decode
+##############################################################
+
+################# ADD UTILITY FUNCTIONS HERE #################
+
+
+
+
+
+##############################################################
+
+def control_logic(sim):
+	"""
+	Purpose:
+	---
+	This function should implement the control logic for the given problem statement
+	You are required to make the robot follow the line to cover all the checkpoints
+	and deliver packages at the correct locations.
+
+	Input Arguments:
+	---
+	`sim`    :   [ object ]
+		ZeroMQ RemoteAPI object
+
+	Returns:
+	---
+	None
+
+	Example call:
+	---
+	control_logic(sim)
+	"""
+	##############  ADD YOUR CODE HERE  ##############
+	e=sim.getObject("/left_joint")
+	r=sim.getObject("/right_joint")
+	m=sim.getObject("/vision_sensor")
+	sim.setJointTargetVelocity(e,4)
+	sim.setJointTargetVelocity(r,4)
+
+	t=1
+	while t:
+		# sim.setJointTargetVelocity(e,1)
+		# sim.setJointTargetVelocity(r,1)
+		# result,auxiliaryValuePacket1,auxiliaryValuePacket2=sim.readVisionSensor(m)
+		result,distance,detectedPoint,detectedObjectHandle,detectedSurfaceNormalVector=sim.handleProximitySensor(m)
+		# imgHandle=sim.readFromVisionSensor(m)
+		img, resX, resY = sim.getVisionSensorCharImage(m)
+		img = np.frombuffer(img, dtype=np.uint8).reshape(resY, resX, 3)
+
+		# In CoppeliaSim images are left to right (x-axis), and bottom to top (y-axis)
+		# (consistent with the axes of vision sensors, pointing Z outwards, Y up)
+		# and color format is RGB triplets, whereas OpenCV uses BGR:
+		img = cv2.flip(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
+
+		cv2.imshow('jjj', img)
+
+	
+
+	##################################################
+
+def read_qr_code(sim):
+	"""
+	Purpose:
+	---
+	This function detects the QR code present in the camera's field of view and
+	returns the message encoded into it.
+
+	Input Arguments:
+	---
+	`sim`    :   [ object ]
+		ZeroMQ RemoteAPI object
+
+	Returns:
+	---
+	`qr_message`   :    [ string ]
+		QR message retrieved from reading QR code
+
+	Example call:
+	---
+	control_logic(sim)
+	"""
+	qr_message = None
+	##############  ADD YOUR CODE HERE  ##############
+
+	##################################################
+	return qr_message
+
+
+######### YOU ARE NOT ALLOWED TO MAKE CHANGES TO THE MAIN CODE BELOW #########
+
+if __name__ == "__main__":
+	client = RemoteAPIClient()
+	sim = client.getObject('sim')	
+
+	try:
+
+		## Start the simulation using ZeroMQ RemoteAPI
+		try:
+			return_code = sim.startSimulation()
+			if sim.getSimulationState() != sim.simulation_stopped:
+				print('\nSimulation started correctly in CoppeliaSim.')
+			else:
+				print('\nSimulation could not be started correctly in CoppeliaSim.')
+				sys.exit()
+
+		except Exception:
+			print('\n[ERROR] Simulation could not be started !!')
+			traceback.print_exc(file=sys.stdout)
+			sys.exit()
+
+		## Runs the robot navigation logic written by participants
+		try:
+			time.sleep(5)
+			control_logic(sim)
+
+		except Exception:
+			print('\n[ERROR] Your control_logic function throwed an Exception, kindly debug your code!')
+			print('Stop the CoppeliaSim simulation manually if required.\n')
+			traceback.print_exc(file=sys.stdout)
+			print()
+			sys.exit()
+
+		
+		## Stop the simulation using ZeroMQ RemoteAPI
+		try:
+			return_code = sim.stopSimulation()
+			time.sleep(0.5)
+			if sim.getSimulationState() == sim.simulation_stopped:
+				print('\nSimulation stopped correctly in CoppeliaSim.')
+			else:
+				print('\nSimulation could not be stopped correctly in CoppeliaSim.')
+				sys.exit()
+
+		except Exception:
+			print('\n[ERROR] Simulation could not be stopped !!')
+			traceback.print_exc(file=sys.stdout)
+			sys.exit()
+
+	except KeyboardInterrupt:
+		## Stop the simulation using ZeroMQ RemoteAPI
+		return_code = sim.stopSimulation()
+		time.sleep(0.5)
+		if sim.getSimulationState() == sim.simulation_stopped:
+			print('\nSimulation interrupted by user in CoppeliaSim.')
+		else:
+			print('\nSimulation could not be interrupted. Stop the simulation manually .')
+			sys.exit()
