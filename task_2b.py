67eecd05b9fc6a613e96e103a5b5988357e67ace
Vnsgu123 <111989739+Vnsgu123@users.noreply.github.com>
2022-11-09 00:28:51 +0530

    Add files via upload

--- task_2b.py
+++ task_2b.py
@@ -75,30 +75,123 @@ def control_logic(sim):
 	control_logic(sim)
 	"""
 	##############  ADD YOUR CODE HERE  ##############
+	defaultIdleFps = sim.getInt32Param(sim.intparam_idle_fps)
+	sim.setInt32Param(sim.intparam_idle_fps, 0)
+
 	e=sim.getObject("/left_joint")
 	r=sim.getObject("/right_joint")
 	m=sim.getObject("/vision_sensor")
-	sim.setJointTargetVelocity(e,4)
-	sim.setJointTargetVelocity(r,4)
 
+	sim.setJointTargetVelocity(e,1)
+	sim.setJointTargetVelocity(r,1)
 	t=1
+	flag4=0
+	flag=0
+	flag1=0
+	flag2=1
 	while t:
-		# sim.setJointTargetVelocity(e,1)
-		# sim.setJointTargetVelocity(r,1)
-		# result,auxiliaryValuePacket1,auxiliaryValuePacket2=sim.readVisionSensor(m)
-		result,distance,detectedPoint,detectedObjectHandle,detectedSurfaceNormalVector=sim.handleProximitySensor(m)
-		# imgHandle=sim.readFromVisionSensor(m)
+		u=0
+		print("run")
 		img, resX, resY = sim.getVisionSensorCharImage(m)
 		img = np.frombuffer(img, dtype=np.uint8).reshape(resY, resX, 3)
-
-		# In CoppeliaSim images are left to right (x-axis), and bottom to top (y-axis)
-		# (consistent with the axes of vision sensors, pointing Z outwards, Y up)
-		# and color format is RGB triplets, whereas OpenCV uses BGR:
 		img = cv2.flip(cv2.cvtColor(img, cv2.COLOR_BGR2RGB), 0)
+		hsv=cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		low_b = np.array([0,0,168])
+		high_b = np.array([172,111,255])
+		yellow_lower = np.array([20, 100, 100])
+		yellow_upper = np.array([30, 255, 255])
+		mask = cv2.inRange(hsv, low_b, high_b)
+		mask1 = cv2.inRange(hsv,yellow_lower,yellow_upper)
+		out = cv2.bitwise_and(img,img, mask= mask)
+		out1 = cv2.bitwise_and(img,img, mask= mask1)
+		contours, hierarchy = cv2.findContours(mask, 1, cv2.CHAIN_APPROX_NONE)
+		contours1, hierarchy1 = cv2.findContours(mask1, 1, cv2.CHAIN_APPROX_NONE)
+		for contour in contours:
+			u=u+1
+			approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+			x,y,w,h=cv2.boundingRect(contour)
+			# print("%%%%%%",w)
+			if w < 35 and w>25 and len(contour) < 350 and len(contour)>200:
+				cv2.drawContours(img, [approx], 0, (0, 0, 255), 5)
+				ll=len(contour)
+				print("----------",ll,w)
+				M = cv2.moments(contour)
+				if M["m00"] !=0 :
+					cx = int(M['m10']/M['m00'])
+					cy = int(M['m01']/M['m00'])
+					print("CX : "+str(cx)+"  CY : "+str(cy))
+				if cx < 245 :
+					sim.setJointTargetVelocity(e,0.5)
+				elif cx > 250 :
+					sim.setJointTargetVelocity(r,0.5)
+				else :
+					sim.setJointTargetVelocity(e,1)
+					sim.setJointTargetVelocity(r,1)
+		q=0
+		flag3=0
+		for contour in contours1:
+			flag3=1
+			flag4=1
+			print(len(contour))
+			q=q+1
+			approx = cv2.approxPolyDP(contour, 0.01* cv2.arcLength(contour, True), True)
+			# if len(contour) >2000:
+			# 	flag=1
+			cv2.drawContours(img, [approx], 0, (0, 0, 255), 5)
+		if q==13 and flag2==1:
+			sim.setJointTargetVelocity(e,0)
+			sim.setJointTargetVelocity(r,0)
+			print("maiyfesysifdisfsfisffi")
+			flag1=1
+			flag2=0
+			# activate_qr_code()
+			# read_qr_code(sim)s
+			# deactivate_qr_code()
 
-		cv2.imshow('jjj', img)
+			sim.setJointTargetVelocity(e,1)
+			sim.setJointTargetVelocity(r,1)
+		print(flag1,cx,cy)
+		# if flag1==1 and cx >220 and cy < 100 and cy > 40 and flag3==0:
+		if flag4==1 and flag3==0:
+			sim.setJointTargetVelocity(e,-1)
+			# print("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+			# y=1
+			# while y:
+			# 	flag4=0
+			# 	# print("runkkk")
+			# sim.setJointTargetVelocity(r,1)
 
 	
+
+
+
+
+		# if u>=7 and cy > 440 and cy <450 :
+		# 	flag=flag+1
+		# if cy > 440 and cy < 450 and u>=7 and flag==1:
+		# 	sim.setJointTargetVelocity(e,0)
+		# 	sim.setJointTargetVelocity(r,0)
+		# 	print("maitrey")
+		# 	# activate_qr_code()
+		# 	# read_qr_code(sim)s
+		# 	# deactivate_qr_code()
+		# 	sim.setJointTargetVelocity(e,1)
+		# 	sim.setJointTargetVelocity(r,1)
+		# if cy > 400 and u==7 and flag==2:
+		# 	print("patel")
+		# 	sim.setJointTargetVelocity(e,-1)
+
+
+
+
+		cv2.imshow('maitrey',img)
+		cv2.imshow('mask',mask)
+		cv2.imshow('mask1',mask1)
+		# cv2.imshow('out',out)
+		cv2.waitKey(1)
+		client.step()  # triggers next simulation step
+
+
 
 	##################################################
 
